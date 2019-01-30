41d09690ff9d5f5d84cc2e92d78a4891df4f0129
Jonathan VUILLEMIN <ekkinox@gmail.com>
2019-01-30 13:36:35 +0100

    Extensions DTO + collection + factory

--- src/Factory/ExtensionFactory.php
+++ src/Factory/ExtensionFactory.php
@@ -6,8 +6,11 @@ use OAT\DependencyResolver\Extension\Extension;
 
 class ExtensionFactory
 {
-    public function create(string $extensionName, string $repositoryName, string $branch = Extension::DEFAULT_BRANCH): Extension
-    {
+    public function create(
+        string $extensionName,
+        string $repositoryName,
+        string $branch = Extension::DEFAULT_BRANCH
+    ): Extension {
         return new Extension($extensionName, $repositoryName, $branch);
     }
 }
