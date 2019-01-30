1c66ebaab4984f7a84eb899ab918dcc45953c305
Jonathan VUILLEMIN <ekkinox@gmail.com>
2019-01-30 14:32:36 +0100

    Autoload extension map

--- src/Factory/ExtensionFactory.php
+++ src/Factory/ExtensionFactory.php
@@ -10,13 +10,6 @@ class ExtensionFactory
 
     public function __construct(array $extensionMap = [])
     {
-        $extensionMap = [
-            'taoQtiItem' => 'oat-sa/extension-tao-itemqti',
-            'taoItems' => 'oat-sa/extension-tao-item',
-            'taoBackOffice' => 'oat-sa/extension-tao-backoffice',
-            'tao' => 'oat-sa/tao-core',
-            'generis' => 'oat-sa/generis',
-        ];
         $this->extensionMap = $extensionMap;
     }
 
